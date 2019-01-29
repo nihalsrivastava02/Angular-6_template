ba6a351c0bb27ed0b2e2b2c9a3221e200aef957f
Nihal Srivastava <43031625+nihalsrivastava02@users.noreply.github.com>
2019-01-29 18:41:29 +0530

    Add files via upload

---
+++ src/app/compile.sh
@@ -0,0 +1,15 @@
+ng new ng6 --style=scss --routing
+#ng6 is a project name.
+cd ng6
+#change directory to ng6.
+ng serve -o
+#localhost port 4200 will be open.
+ng generate component sidebar
+#sidebar is added.
+ng generate component posts
+ng generate component users
+ng generate component details
+ng generate service data
+#data services will be created.
+#USEFULL LINKS.
+#https://coursetro.com/posts/code/154/Angular-6-Tutorial---Learn-Angular-6-in-this-Crash-Course.
