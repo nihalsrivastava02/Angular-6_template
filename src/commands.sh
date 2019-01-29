3885c75d74f084081ee30d4b1f01d71d3923f80d
Nihal Srivastava <43031625+nihalsrivastava02@users.noreply.github.com>
2019-01-29 18:38:51 +0530

    Add files via upload

---
+++ src/commands.sh
@@ -0,0 +1,9 @@
+ng new ng6 --style=scss --routing
+# ng6 project will be created.
+ng serve -o
+#local host port 4200 will be open.
+ng generate component sidebar
+#sidebar component will be created.
+ng generate component posts
+ng generate component users
+ng generate component details
