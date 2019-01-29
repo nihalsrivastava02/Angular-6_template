5f54eb9811c1266d88bd0dd0af6cb9cab166a32d
Nihal Srivastava <43031625+nihalsrivastava02@users.noreply.github.com>
2019-01-29 18:44:10 +0530

    Add files via upload

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit {
+users$: Object;
+  
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.data.getUsers().subscribe(
+      data => this.users$ = data 
+    );
+  }
+
+}
